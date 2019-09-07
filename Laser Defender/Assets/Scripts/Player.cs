d18071e3284e474500b52ae8da5493a8e622691f
Josenn <josenn44@gmail.com>
2019-09-07 12:29:58 -0500

    Added player movement

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -5,21 +5,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        MoveHorizontal();
+        MoveVertical();
     }
 
-    private void Move()
+    private void MoveVertical()
     {
-        var deltaX = Input.GetAxis("Horizontal");
+        //delta is referring to a change in where we are and where we want to be
+
+        //TODO lots of refactoring and commenting (on other projects too)
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(transform.position.x, newYPos);
+    }
+
+    //might need to change and merge these 2 later who knows
+    private void MoveHorizontal()
+    {
+        //delta is referring to a change in where we are and where we want to be
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var newXPos = transform.position.x + deltaX;
+        transform.position = new Vector2(newXPos, transform.position.y);
     }
 }
