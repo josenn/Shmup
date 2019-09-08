091af8df8680660a02403b6018ce8fe595cf65f4
Josenn <josenn44@gmail.com>
2019-09-08 14:48:52 -0500

    Set up horizontal and vertical bounds

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -6,37 +6,46 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
+        SetUpMoveBoundaries();
+    }
 
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveHorizontal();
-        MoveVertical();
-    }
-
-    private void MoveVertical()
-    {
-        //delta is referring to a change in where we are and where we want to be
-
-        //TODO lots of refactoring and commenting (on other projects too)
-
-        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
-        transform.position = new Vector2(transform.position.x, newYPos);
+        Move();
     }
 
     //might need to change and merge these 2 later who knows
-    private void MoveHorizontal()
+    private void Move()
     {
         //delta is referring to a change in where we are and where we want to be
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = transform.position.x + deltaX;
-        transform.position = new Vector2(newXPos, transform.position.y);
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp((transform.position.x + deltaX), xMin, xMax);
+        var newYPos = Mathf.Clamp((transform.position.y + deltaY), yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+
+        //TODO lots of refactoring and commenting (on other projects too)
     }
 }
