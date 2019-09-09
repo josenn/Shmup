8808942d4e4216cd50b7191515159cde741d678e
Josenn <josenn44@gmail.com>
2019-09-09 08:23:30 -0500

    Began enemy pathing

---
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(waypointIndex <= waypoints.Count - 1)
+        {
+
+        }
+    }
+}
