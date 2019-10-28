a8ad6887048f3ee14f1d87ae3ffcbf00fb3f1e48
Josenn <josenn44@gmail.com>
2019-10-27 20:15:07 -0500

    fixed pathing

---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int i = 0; i < waveConfig.GetNumberOfEnemies(); i++)
+        {
+            //quaternion identity just says "we don't need rotation just use the rotation you already have.
+            Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWaypoints()[0].transform.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns() * waveConfig.GetSpawnRandomFactor());
+        }
+    }
+}
