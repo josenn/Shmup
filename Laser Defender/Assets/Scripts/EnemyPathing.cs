f5369cb2be93861aae4f577e513c466b293844f6
Josenn <josenn44@gmail.com>
2019-10-27 20:07:27 -0500

    Revert "Added enemy pathing"
    This reverts commit b22fa0d8df7d909824d2fac5fcd9c3b12ed25406.

--- Laser Defender/Assets/Scripts/EnemyPathing.cs
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
-    [SerializeField] WaveConfig waveConfig;
-    List<Transform> waypoints;
+    [SerializeField] List<Transform> waypoints;
     [SerializeField] float moveSpeed = 2f;
     int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].transform.position;
     }
 
@@ -21,27 +19,7 @@ public class EnemyPathing : MonoBehaviour
     {
         if(waypointIndex <= waypoints.Count - 1)
         {
-            Move();
-        }
-    }
 
-    private void Move()
-    {
-        //where we want to go to
-        var targetPosition = waypoints[waypointIndex].transform.position;
-
-        //how fast we want to move
-        var movementThisFrame = moveSpeed * Time.deltaTime;
-
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
-
-        if (transform.position == targetPosition)
-        {
-            waypointIndex++;
-        }
-        if (waypointIndex == waypoints.Count)
-        {
-            Destroy(gameObject);
         }
     }
 }
