1589c595711649b5d3eb0ae7e6445450b61079d5
Josenn <josenn44@gmail.com>
2020-03-16 13:29:29 -0500

    Added description of the class
    I added a description for this class (I plan on doing this for others too) and cleaned up white space

--- Laser Defender/Assets/Scripts/DamageDealer.cs
+++ Laser Defender/Assets/Scripts/DamageDealer.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections;
+﻿/* This script is the damage dealer of our game
+ * 
+ * It's pretty simple really
+ * 
+ * This class has a certain amount of damage that can be dealt
+ * 
+ * A function to get said damage (so other classes can know how much damage to take)
+ * 
+ * And a function to process the hit of an object
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +17,7 @@ public class DamageDealer : MonoBehaviour
 {
     [SerializeField] int damage = 0;
 
-   public int GetDamage() { return damage;  }
+    public int GetDamage() { return damage;  }
 
     public void Hit()
     {
