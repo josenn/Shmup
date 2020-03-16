931f25c6cd74c0f5217a96107a984e1f05a32835
Josenn <josenn44@gmail.com>
2020-03-16 13:32:35 -0500

    Added headers and cleaned up comments

--- Laser Defender/Assets/Scripts/Enemy.cs
+++ Laser Defender/Assets/Scripts/Enemy.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Enemy")]
     [SerializeField] float health = 1000F;
     [SerializeField] float projectileSpeed = 5f;
     [SerializeField] float shotTimer;
     [SerializeField] float minTimeBetweenShots = 0.2f;
     [SerializeField] float maxTimeBetweenShots = 3f;
 
+    [Header("Projectile")]
     // References
     [SerializeField] GameObject laserPrefab;
 
@@ -27,7 +29,7 @@ public class Enemy : MonoBehaviour
 
     private void CountDownAndShoot()
     {
-        //our shot counter needs to be going down for however long our frame takes
+        // Our shot counter needs to be going down for however long our frame takes
         shotTimer -= Time.deltaTime; //the time that our frame takes
         
         if (shotTimer <= 0f)
@@ -46,13 +48,13 @@ public class Enemy : MonoBehaviour
 
     private void Fire()
     {
-        //creating this as a gameobject
+        // Creating this as a gameobject
         GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
 
         laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
     }
 
-    //other means the game object that collided with this thing
+    // "Other" means the game object that collided with this thing
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
@@ -63,7 +65,7 @@ public class Enemy : MonoBehaviour
     {
         health -= damageDealer.GetDamage();
 
-        //to fix cases of going into the negatives by mistake we add the < sign
+        // To fix cases of going into the negatives by mistake we add the < sign
         if (health <= 0)
         {
             Destroy(gameObject);
