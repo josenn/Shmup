9378cc453ec07c75217c0c741f0f0a19f467cbcb
Josenn <josenn44@gmail.com>
2020-03-20 14:27:27 -0500

    Added class explanation
    I added an explanation of the class at the top of the file

--- Laser Defender/Assets/Scripts/BackgroundScroller.cs
+++ Laser Defender/Assets/Scripts/BackgroundScroller.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections;
+﻿/* This class is used for scrolling the background
+ *
+ * It has a set amount of scroll speed and ways to access the material
+ *
+ * And material offset of the object that it is attached to
+*/
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,8 +15,6 @@ public class BackgroundScroller : MonoBehaviour
     [SerializeField] float backgroundScrollSpeed = 0.5f;
     Material material;
     Vector2 offset;
-
-
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +28,7 @@ public class BackgroundScroller : MonoBehaviour
     void Update()
     {
         // mainTextureOffset is how we get access to the main texture offset of the object's material
+        // This allows us to make the textue move
         material.mainTextureOffset += (offset * Time.deltaTime);
     }
 }
