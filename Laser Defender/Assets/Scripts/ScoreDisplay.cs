ad5fcfb3b54c437b2475df10aa05e23ceefc2750
Josenn <josenn44@gmail.com>
2020-04-06 19:50:52 -0500

    Added a way to display the player's score

---
+++ Laser Defender/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,29 @@
+﻿/* This class determines the display of the player's score
+ *
+ * Pretty simple so there's not much to say on it
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    // The score text
+    Text scoreText;
+    GameSession gameSession;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        gameSession = FindObjectOfType<GameSession>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = gameSession.GetScore().ToString();
+    }
+}
