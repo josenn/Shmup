c1e9a8c0cd82f3152edd2c91aeb045d5fde90f8d
Josenn <josenn44@gmail.com>
2020-04-06 19:53:39 -0500

    Added a class for managing the game session
    This class only manages the score right now but could be made to manage other things as needed

---
+++ Laser Defender/Assets/Scripts/GameSession.cs
@@ -0,0 +1,57 @@
+﻿/* This class controls game elements such as the players score
+ * 
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    // Information about the game session
+    // Current Score
+    int score = 0;
+
+    // Awake is the very first function in the execution order
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    // This manner of singleton that I keep implementing could be a nice code snippet
+    // TODO makes this a code snippet and store it somewhere
+    private void SetUpSingleton()
+    {
+        // The number of game sessions in the scene
+        int numberOfGameSessions = FindObjectsOfType<GameSession>().Length;
+
+        // If there is more than 1 game session destroy all other game sessions or don't destroy yourself
+        if (numberOfGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Gets the score
+    public int GetScore()
+    {
+        return score;
+    }
+    
+    // Adds scoreValue to the score
+    public void AddToScore(int scoreValue)
+    {
+        score += scoreValue;
+    }
+
+    // Resets the game
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
