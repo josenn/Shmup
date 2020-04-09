e8e83926000371f9babb2a884f71e753e5ea1c4e
Josenn <josenn44@gmail.com>
2020-04-09 16:55:30 -0500

    Added class for rotation of projectiles

---
+++ Laser Defender/Assets/Scripts/Spinner.cs
@@ -0,0 +1,17 @@
+﻿/* This class controls the rotation of certain enemy projectiles */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] float speedOfSpin = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the projectile (note in 2-D space z is the rotation we want)
+        transform.Rotate(0, 0, speedOfSpin * Time.deltaTime);
+    }
+}
